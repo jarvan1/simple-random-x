fd8d0b1a6aaa8b308ad5531bf5135ba59e7ca219
xinming.xu <xuxm@8btc.com>
2024-04-02 16:41:38 +0800

    add

---
+++ index.js
@@ -0,0 +1,5 @@
+const random = require('./lib/random')
+module.exports = random
+
+// Support by : https://github.com/voucherifyio/voucher-code-generator-js
+// Created by : https://github.com/zaadevofc
